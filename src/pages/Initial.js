fb504a1fd64646c6159195930fb7b7b0957d7aed
Kushan Nalinka <kushannalinka572@gmail.com>
2024-08-28 08:50:01 +0530

    News Component
    News Component Content

--- src/pages/Initial.js
+++ src/pages/Initial.js
@@ -250,6 +250,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../components/HomeNavbar';
 import RCCRIC from '../assets/images/bg.png';
 import CroppedImage from '../assets/images/Cropped.png';
+import NewsSlider from '../components/NewsSlider';
 
 const HomePage = () => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
@@ -264,6 +265,7 @@ const HomePage = () => {
   }, []);
 
   return (
+    <>
     <div className="relative h-screen w-full">
       <div 
         className={`absolute inset-0 ${isImageLoaded ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}
@@ -285,7 +287,7 @@ const HomePage = () => {
               style={{ 
                 fontSize: '11rem', 
                 lineHeight: '1', 
-                animationDelay: '0.5s', 
+                animationDelay: '1.5s', 
                 background: 'linear-gradient(to right, #0E1E79, #1A68DC)', // Gradient colors
                 WebkitBackgroundClip: 'text', // Clip the background to the text
                 WebkitTextFillColor: 'transparent', // Fill the text with the gradient
@@ -303,13 +305,13 @@ const HomePage = () => {
           />
 
           {/* "COLLEGE" Text on Top of Cropped Image */}
-          <div className="absolute bottom-0 w-full text-center z-30">
+          <div className="absolute bottom-0 w-full text-center z-20">
             <div 
               className="font-bold text-[#FFCB05] animate-slide-up"
               style={{ 
                 fontSize: '11rem', 
                 lineHeight: '1', 
-                animationDelay: '0.5s' 
+                animationDelay: '1.5s' 
               }} 
             >
               COLLEGE
@@ -318,6 +320,8 @@ const HomePage = () => {
         </div>
       </div>
     </div>
+    <NewsSlider />
+    </>
   );
 };
 
